23bb9fa8520e2dbb142fbd8c317ab9c8952cb7ce
SyedUIBK <ashfire78@gmail.com>
2024-12-07 00:05:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def generate_complete_graph(num_nodes, weight_range=(1, 100)):
+  
     G = nx.complete_graph(num_nodes)
     for u, v in G.edges():
         G.edges[u, v]['weight'] = random.randint(*weight_range)
     return G
 
 def plot_graph_step(G, tour, current_node, pos):
+ 
     plt.clf()
     nx.draw(G, pos, with_labels=True, node_color='red', node_size=500)
     path_edges = list(zip(tour, tour[1:]))
@@ -18,7 +20,7 @@ def plot_graph_step(G, tour, current_node, pos):
     edge_labels = nx.get_edge_attributes(G, name='weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
-    plt.pause(100)
+    plt.pause(0.6)  # Shorter pause for smoother visualization
 
 def calculate_tour_cost(G, tour):
     return sum(G[tour[i]][tour[i + 1]]['weight'] for i in range(len(tour) - 1))
@@ -27,7 +29,7 @@ def nearest_neighbor_tsp(G, start_node=None):
     if start_node is None:
         start_node = random.choice(list(G.nodes))
     
-    pos = nx.spring_layout(G)
+    pos = nx.circular_layout(G)  
     plt.ion()
     plt.show()
 
@@ -46,10 +48,10 @@ def nearest_neighbor_tsp(G, start_node=None):
         current_node = next_node
         plot_graph_step(G, tour, current_node, pos)
 
-    tour.append(start_node)
+    tour.append(start_node)  
     plot_graph_step(G, tour, current_node, pos)
 
-    print(tour)
+    print("Tour:", tour)
     tour_cost = calculate_tour_cost(G, tour)
     print(f'Construction heuristic Tour Cost: {tour_cost}')
 
@@ -57,5 +59,5 @@ def nearest_neighbor_tsp(G, start_node=None):
     plt.show()
 
 if __name__ == '__main__':
-    G = generate_complete_graph(10)
+    G = generate_complete_graph(5)
     nearest_neighbor_tsp(G, start_node=0)
